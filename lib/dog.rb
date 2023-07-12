c0b46a913a09153a827f8bd92e4fda7e80882975
= <brianwaweru627@gmail.com>
2023-07-13 02:22:17 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+    def name=(dog_name)
+      @name = dog_name
+    end
+  
+    def name
+      @name
+    end
 
-end
+    def breed=(dog_breed)
+        @breed = dog_breed
+      end
+    
+      def breed
+        @breed
+      end
+  end
+  
+#   lassie = Dog.new
+#   lassie.name = "Lassie"
+  
+#   puts lassie.name
